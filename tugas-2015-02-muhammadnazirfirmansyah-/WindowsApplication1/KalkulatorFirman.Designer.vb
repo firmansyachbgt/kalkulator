a3eb9528646c23dd8b0abd49f234b58e9debfd06
iwanswidodo <iwanf2509@gmail.com>
2015-10-10 19:59:59 +0700

    M Nazir firmansyah_1531143003

---
+++ tugas-2015-02-muhammadnazirfirmansyah-/WindowsApplication1/KalkulatorFirman.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class KalkulatorFirman
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.kal1 = New System.Windows.Forms.Button
+        Me.kal4 = New System.Windows.Forms.Button
+        Me.kal7 = New System.Windows.Forms.Button
+        Me.kaltambah = New System.Windows.Forms.Button
+        Me.kal2 = New System.Windows.Forms.Button
+        Me.kal5 = New System.Windows.Forms.Button
+        Me.kal8 = New System.Windows.Forms.Button
+        Me.kal0 = New System.Windows.Forms.Button
+        Me.kal3 = New System.Windows.Forms.Button
+        Me.kal6 = New System.Windows.Forms.Button
+        Me.kal9 = New System.Windows.Forms.Button
+        Me.kalkurang = New System.Windows.Forms.Button
+        Me.kalbagi = New System.Windows.Forms.Button
+        Me.kalkali = New System.Windows.Forms.Button
+        Me.kalsamadengan = New System.Windows.Forms.Button
+        Me.kaldisplay = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(261, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'kal1
+        '
+        Me.kal1.Location = New System.Drawing.Point(39, 138)
+        Me.kal1.Name = "kal1"
+        Me.kal1.Size = New System.Drawing.Size(75, 23)
+        Me.kal1.TabIndex = 3
+        Me.kal1.Text = "1"
+        Me.kal1.UseVisualStyleBackColor = True
+        '
+        'kal4
+        '
+        Me.kal4.Location = New System.Drawing.Point(39, 167)
+        Me.kal4.Name = "kal4"
+        Me.kal4.Size = New System.Drawing.Size(75, 23)
+        Me.kal4.TabIndex = 4
+        Me.kal4.Text = "4"
+        Me.kal4.UseVisualStyleBackColor = True
+        '
+        'kal7
+        '
+        Me.kal7.Location = New System.Drawing.Point(39, 196)
+        Me.kal7.Name = "kal7"
+        Me.kal7.Size = New System.Drawing.Size(75, 23)
+        Me.kal7.TabIndex = 5
+        Me.kal7.Text = "7"
+        Me.kal7.UseVisualStyleBackColor = True
+        '
+        'kaltambah
+        '
+        Me.kaltambah.Location = New System.Drawing.Point(39, 232)
+        Me.kaltambah.Name = "kaltambah"
+        Me.kaltambah.Size = New System.Drawing.Size(75, 23)
+        Me.kaltambah.TabIndex = 6
+        Me.kaltambah.Text = "+"
+        Me.kaltambah.UseVisualStyleBackColor = True
+        '
+        'kal2
+        '
+        Me.kal2.Location = New System.Drawing.Point(132, 138)
+        Me.kal2.Name = "kal2"
+        Me.kal2.Size = New System.Drawing.Size(75, 23)
+        Me.kal2.TabIndex = 7
+        Me.kal2.Text = "2"
+        Me.kal2.UseVisualStyleBackColor = True
+        '
+        'kal5
+        '
+        Me.kal5.Location = New System.Drawing.Point(132, 167)
+        Me.kal5.Name = "kal5"
+        Me.kal5.Size = New System.Drawing.Size(75, 23)
+        Me.kal5.TabIndex = 8
+        Me.kal5.Text = "5"
+        Me.kal5.UseVisualStyleBackColor = True
+        '
+        'kal8
+        '
+        Me.kal8.Location = New System.Drawing.Point(132, 196)
+        Me.kal8.Name = "kal8"
+        Me.kal8.Size = New System.Drawing.Size(75, 23)
+        Me.kal8.TabIndex = 9
+        Me.kal8.Text = "8"
+        Me.kal8.UseVisualStyleBackColor = True
+        '
+        'kal0
+        '
+        Me.kal0.Location = New System.Drawing.Point(132, 232)
+        Me.kal0.Name = "kal0"
+        Me.kal0.Size = New System.Drawing.Size(75, 23)
+        Me.kal0.TabIndex = 10
+        Me.kal0.Text = "0"
+        Me.kal0.UseVisualStyleBackColor = True
+        '
+        'kal3
+        '
+        Me.kal3.Location = New System.Drawing.Point(229, 138)
+        Me.kal3.Name = "kal3"
+        Me.kal3.Size = New System.Drawing.Size(75, 23)
+        Me.kal3.TabIndex = 11
+        Me.kal3.Text = "3"
+        Me.kal3.UseVisualStyleBackColor = True
+        '
+        'kal6
+        '
+        Me.kal6.Location = New System.Drawing.Point(229, 167)
+        Me.kal6.Name = "kal6"
+        Me.kal6.Size = New System.Drawing.Size(75, 23)
+        Me.kal6.TabIndex = 12
+        Me.kal6.Text = "6"
+        Me.kal6.UseVisualStyleBackColor = True
+        '
+        'kal9
+        '
+        Me.kal9.Location = New System.Drawing.Point(229, 196)
+        Me.kal9.Name = "kal9"
+        Me.kal9.Size = New System.Drawing.Size(75, 23)
+        Me.kal9.TabIndex = 13
+        Me.kal9.Text = "9"
+        Me.kal9.UseVisualStyleBackColor = True
+        '
+        'kalkurang
+        '
+        Me.kalkurang.Location = New System.Drawing.Point(229, 231)
+        Me.kalkurang.Name = "kalkurang"
+        Me.kalkurang.Size = New System.Drawing.Size(75, 23)
+        Me.kalkurang.TabIndex = 14
+        Me.kalkurang.Text = "-"
+        Me.kalkurang.UseVisualStyleBackColor = True
+        '
+        'kalbagi
+        '
+        Me.kalbagi.Location = New System.Drawing.Point(229, 262)
+        Me.kalbagi.Name = "kalbagi"
+        Me.kalbagi.Size = New System.Drawing.Size(75, 23)
+        Me.kalbagi.TabIndex = 15
+        Me.kalbagi.Text = ":"
+        Me.kalbagi.UseVisualStyleBackColor = True
+        '
+        'kalkali
+        '
+        Me.kalkali.Location = New System.Drawing.Point(39, 262)
+        Me.kalkali.Name = "kalkali"
+        Me.kalkali.Size = New System.Drawing.Size(75, 23)
+        Me.kalkali.TabIndex = 16
+        Me.kalkali.Text = "x"
+        Me.kalkali.UseVisualStyleBackColor = True
+        '
+        'kalsamadengan
+        '
+        Me.kalsamadengan.Location = New System.Drawing.Point(132, 262)
+        Me.kalsamadengan.Name = "kalsamadengan"
+        Me.kalsamadengan.Size = New System.Drawing.Size(75, 23)
+        Me.kalsamadengan.TabIndex = 18
+        Me.kalsamadengan.Text = "="
+        Me.kalsamadengan.UseVisualStyleBackColor = True
+        '
+        'kaldisplay
+        '
+        Me.kaldisplay.Location = New System.Drawing.Point(39, 82)
+        Me.kaldisplay.Multiline = True
+        Me.kaldisplay.Name = "kaldisplay"
+        Me.kaldisplay.ReadOnly = True
+        Me.kaldisplay.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.kaldisplay.Size = New System.Drawing.Size(205, 31)
+        Me.kaldisplay.TabIndex = 19
+        Me.kaldisplay.Text = "0"
+        '
+        'KalkulatorFirman
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(391, 346)
+        Me.Controls.Add(Me.kaldisplay)
+        Me.Controls.Add(Me.kalsamadengan)
+        Me.Controls.Add(Me.kalkali)
+        Me.Controls.Add(Me.kalbagi)
+        Me.Controls.Add(Me.kalkurang)
+        Me.Controls.Add(Me.kal9)
+        Me.Controls.Add(Me.kal6)
+        Me.Controls.Add(Me.kal3)
+        Me.Controls.Add(Me.kal0)
+        Me.Controls.Add(Me.kal8)
+        Me.Controls.Add(Me.kal5)
+        Me.Controls.Add(Me.kal2)
+        Me.Controls.Add(Me.kaltambah)
+        Me.Controls.Add(Me.kal7)
+        Me.Controls.Add(Me.kal4)
+        Me.Controls.Add(Me.kal1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "KalkulatorFirman"
+        Me.Text = "Kangkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents kal1 As System.Windows.Forms.Button
+    Friend WithEvents kal4 As System.Windows.Forms.Button
+    Friend WithEvents kal7 As System.Windows.Forms.Button
+    Friend WithEvents kaltambah As System.Windows.Forms.Button
+    Friend WithEvents kal2 As System.Windows.Forms.Button
+    Friend WithEvents kal5 As System.Windows.Forms.Button
+    Friend WithEvents kal8 As System.Windows.Forms.Button
+    Friend WithEvents kal0 As System.Windows.Forms.Button
+    Friend WithEvents kal3 As System.Windows.Forms.Button
+    Friend WithEvents kal6 As System.Windows.Forms.Button
+    Friend WithEvents kal9 As System.Windows.Forms.Button
+    Friend WithEvents kalkurang As System.Windows.Forms.Button
+    Friend WithEvents kalbagi As System.Windows.Forms.Button
+    Friend WithEvents kalkali As System.Windows.Forms.Button
+    Friend WithEvents kalsamadengan As System.Windows.Forms.Button
+    Friend WithEvents kaldisplay As System.Windows.Forms.TextBox
+
+End Class
---
+++ tugas-2015-02-muhammadnazirfirmansyah-/WindowsApplication1/KalkulatorFirman.vb
@@ -0,0 +1,283 @@
+﻿Public Class KalkulatorFirman
+    Dim tembak1 As String = ""
+
+    Dim tembak2 As String = ""
+
+    Dim hasil As Decimal = 0
+
+    Dim tambah As Boolean = False
+
+    Dim kurang As Boolean = False
+
+    Dim kali As Boolean = False
+
+    Dim bagi As Boolean = False
+    Private Sub KalkulatorFirman_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub kal1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal1.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "1"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "1"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+    Private Sub kal2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal2.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "2"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "2"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal3.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "3"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "3"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal4.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "4"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "4"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal5.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "5"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "5"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal6.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "6"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "6"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal7.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "7"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "7"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal8.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "8"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "8"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal9.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "9"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "9"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kal0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kal0.Click
+        If (tambah = False And kurang = False And kali = False And bagi = False) Then
+
+            tembak1 += "0"
+
+            kaldisplay.Text = tembak1
+
+        Else
+
+            tembak2 += "0"
+
+            kaldisplay.Text = tembak2
+
+        End If
+    End Sub
+
+    Private Sub kalsamadengan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kalsamadengan.Click
+        If tambah = True Then
+
+            hasil = Val(tembak1) + Val(tembak2)
+
+            kaldisplay.Text = hasil
+
+            tembak1 = hasil
+
+            tembak2 = ""
+
+            tambah = False
+
+        ElseIf kurang = True Then
+
+            hasil = Val(tembak1) - Val(tembak2)
+
+            kaldisplay.Text = hasil
+
+            tembak1 = hasil
+
+            tembak2 = ""
+
+            kurang = False
+
+        ElseIf kali = True Then
+
+            hasil = Val(tembak1) * Val(tembak2)
+
+            kaldisplay.Text = hasil
+
+            tembak1 = hasil
+
+            tembak2 = ""
+
+            kali = False
+
+        ElseIf bagi = True Then
+
+            If tembak2 = "0" Then
+
+                kaldisplay.Text = "Error"
+
+                tembak2 = ""
+
+            Else
+
+                hasil = Val(tembak1) / Val(tembak2)
+
+                kaldisplay.Text = hasil
+
+                tembak1 = hasil
+
+                tembak2 = ""
+
+                bagi = False
+
+            End If
+
+        End If
+    End Sub
+
+    Private Sub kalkali_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kalkali.Click
+        kaldisplay.Text = "x"
+
+        kali = True
+    End Sub
+
+    Private Sub kaltambah_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kaltambah.Click
+        kaldisplay.Text = "+"
+
+        tambah = True
+    End Sub
+
+    Private Sub kalkurang_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kalkurang.Click
+        kaldisplay.Text = "-"
+
+        kurang = True
+    End Sub
+
+    Private Sub kalbagi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kalbagi.Click
+        kaldisplay.Text = ":"
+
+        kurang = True
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        tembak1 = ""
+
+        tembak2 = ""
+
+        kaldisplay.Text = "0"
+
+        kali = False
+
+        bagi = False
+
+        kurang = False
+
+        tambah = False
+    End Sub
+
+    Private Sub kaldisplay_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles kaldisplay.TextChanged
+
+    End Sub
+End Class
